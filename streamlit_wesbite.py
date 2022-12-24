829700a87c78edc7886b495ec6814a2d5e049b98
h-rodrigo <121348265+h-rodrigo@users.noreply.github.com>
2022-12-24 10:09:40 -0500

    Update streamlit_wesbite.py

--- streamlit_wesbite.py
+++ streamlit_wesbite.py
@@ -8,9 +8,7 @@ Author: Hiru Rodrigo                          "
 import pandas as pd 
 import streamlit as st
 import plotly.graph_objects as go
-from PIL import Image
 import plotly.express as px
-import pickle
 from pathlib import Path 
 
 def main(): 
